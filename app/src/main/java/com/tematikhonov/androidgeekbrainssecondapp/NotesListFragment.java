22c7d6a2c9fb9e70a064c9f290d0f2d3a3895bba
buthed <temaTihonov@yandex.ru>
2021-05-17 18:58:03 +0300

    our and half of rework

--- app/src/main/java/com/tematikhonov/androidgeekbrainssecondapp/NotesListFragment.java
+++ app/src/main/java/com/tematikhonov/androidgeekbrainssecondapp/NotesListFragment.java
@@ -2,63 +2,64 @@ package com.tematikhonov.androidgeekbrainssecondapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NotesListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NotesListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public NotesListFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NotesListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NotesListFragment newInstance(String param1, String param2) {
-        NotesListFragment fragment = new NotesListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+//    public static NotesListFragment newInstance(String param1, String param2) {
+//        NotesListFragment fragment = new NotesListFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+//
+//
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//        }
+//    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notes_list, container, false);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initList(view);
+    }
+
+    private void initList(View view) {
+        LinearLayout linearLayout = (LinearLayout) view;
+        String[] notes_name = getResources().getStringArray(R.array.notes_name);
+        for (int i = 0; i < notes_name.length; i++) {
+            String noteName = notes_name[i];
+            TextView textView = new TextView(getContext());
+            textView.setText(noteName);
+            textView.setTextSize(30);
+            linearLayout.addView(textView);
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes_list, container, false);
-    }
+
 }
