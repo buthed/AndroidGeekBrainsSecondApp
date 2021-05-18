5de5199931a0ef5b1753f2d2181250db996e89d4
buthed <temaTihonov@yandex.ru>
2021-05-18 14:52:02 +0300

    before adding note cla

---
+++ app/src/main/java/com/tematikhonov/androidgeekbrainssecondapp/Note.java
@@ -0,0 +1,11 @@
+package com.tematikhonov.androidgeekbrainssecondapp;
+
+public class Note {
+    private String name;
+    private String description;
+
+    public Note(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
--- app/src/main/java/com/tematikhonov/androidgeekbrainssecondapp/NoteFragment.java
+++ app/src/main/java/com/tematikhonov/androidgeekbrainssecondapp/NoteFragment.java
@@ -38,12 +38,17 @@ public class NoteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] notes_name = getResources().getStringArray(R.array.notes_name);
         String[] notes_description = getResources().getStringArray(R.array.notes_description);
+        String[] notes_date = getResources().getStringArray(R.array.notes_date);
         View view = inflater.inflate(R.layout.fragment_note, container, false);
 
-        TextView textView = view.findViewById(R.id.noteView);
-        textView.setText(notes_description[index]);
-
+        TextView tvName = view.findViewById(R.id.noteName);
+        TextView tvDescription = view.findViewById(R.id.noteDescription);
+        TextView tvDate = view.findViewById(R.id.noteDate);
+        tvName.setText(notes_name[index]);
+        tvDescription.setText(notes_description[index]);
+        tvDate.setText(notes_date[index]);
         return view;
     }
 }
