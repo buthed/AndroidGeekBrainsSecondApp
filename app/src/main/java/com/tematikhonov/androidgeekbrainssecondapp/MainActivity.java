5ec9c1c81c94f20bdd84100a7dbd00233ad76424
buthed <temaTihonov@yandex.ru>
2021-05-26 01:41:34 +0300

    With landscape mode and menu, w/o second activity

--- app/src/main/java/com/tematikhonov/androidgeekbrainssecondapp/MainActivity.java
+++ app/src/main/java/com/tematikhonov/androidgeekbrainssecondapp/MainActivity.java
@@ -1,7 +1,18 @@
 package com.tematikhonov.androidgeekbrainssecondapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
+    }
+
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.action_settings:
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "Пора покормить кота!", Toast.LENGTH_SHORT);
+                        toast.show();
+                        return true;
+                    case R.id.action_main:
+                        Toast toast1 = Toast.makeText(getApplicationContext(),
+                                "Пора покормить кота!1", Toast.LENGTH_SHORT);
+                        toast1.show();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    private Toolbar initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        return toolbar;
     }
 }
